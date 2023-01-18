6185ce98221816f712d7235bc3218ecf92809017
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-01-18 20:38:22 +0100

    implementazione header con navabar e jumbo

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,56 +1,70 @@
 <script>
+import jumboText from './jumboText.vue';
+
 export default{
   name: 'AppHeader',
   components:{
+    jumboText,
     
-
   }
 }
 </script>
 
 <template>
-    <div class="container">
-        <div class="row d-flex justify-content-between align-items-center mt-3">
-            <div class="col-5 ">
-                <div class="d-flex align-items-center">
-                    <img src="../assets/images/avada-charity-logo.png" alt="logo" class="logo">
+    <div class="bg ">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container d-flex justify-content-between">
+              <div class="logo">
+                 <img src="../assets/images/avada-charity-logo.png"  class="logo" alt="logo">        
+              </div> 
+              <div class="d-flex">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                       <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse flex-grow-0 " id="navbarScroll">
+                        <div class="navbar-nav text-light">
+                          <a class="nav-link text-light">HOME</a>
+                          <a class="nav-link text-light">MISSION</a>
+                          <a class="nav-link text-light">CAUSES</a>
+                          <a class="nav-link text-light">JOURNAL</a>
+                        </div>
+                    </div>
+                    <div class="d-none d-lg-block">
+                        <button class="bottone  btn-secondary  my-1">
+                            DONATE
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="col-7 d-flex align-items-center">
-                <ul>
-                    <li>HOME</li>
-                    <li>MISSION</li>
-                    <li>CAUSES</li>
-                    <li>JOURNAL</li>
-                </ul>
-        
-                <button class="ms-4">DONATE</button>
-            </div>
+        </nav>
+        <div class="container">
+            <jumboText/>
         </div>
-    </div>
-    
+    </div> 
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general' as * ;
 @use '../styles/partials/variables' as * ;
+
+.bg{
+    background-image: url(../assets/images/home-page-slider.jpg);
+    height: 700px;
+    background-size: cover;
+}
 .logo{
     width: 200px;
 }
-
-button{
+button,
+.bottone{
     background-color: $link-color;
     color: $main-color;
     padding: .5rem 1rem;
+    margin-left: 2rem;
 }
 
-.bf{
-    display: inline-block;
-}
-li{
-    display: inline-block;
-    list-style: none;
-    margin-left: 3rem;
+a{
+   margin-left: 2rem;
 }
 
 
---
+++ src/components/jumboText.vue
@@ -0,0 +1,48 @@
+<script>
+export default{
+  name: 'jumboText',
+  components:{
+    
+
+  }
+}
+</script>
+
+<template>
+    <div class="row  d-flex justify-content-center align-items-center">
+        <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 text-light text-center">
+            <h1>
+                MAKE A DIFFERENCE
+            </h1>
+            <p>as long as poverty, injustice & inequality persist, none of us can truly rest</p>
+            <div>
+                <button class="white">CUR MISSION</button>
+                <button class="yellow">DONATE NOW</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general' as * ;
+@use '../styles/partials/variables' as * ;
+.row{
+    height: 600px;
+}
+
+button{
+    border: solid  1px;
+    padding: .3rem 1rem;
+    margin: 1rem;
+    background-color: transparent;
+}
+
+.white{
+    border-color: $main-color;
+    color: $main-color;
+}
+.yellow{
+    border-color: $link-color;
+    color: $link-color;
+}
+</style>
